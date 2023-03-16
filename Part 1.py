7595af29b77ad15fb6eca6c183549f81eb6ecc09
Ross <23426097+Rlbregant@users.noreply.github.com>
2023-03-15 17:05:41 -0700

    Initial commit

---
+++ Part 1.py
@@ -0,0 +1,35 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+test_dups = ["zzz", "dog", "bookkeeper", "subdermatoglyphic", "subdermatoglyphics"]
+
+test_miss = ["zzz", "subdermatoglyphic", "the quick brown fox jumps over the lazy dog"]
+
+
+def histogram(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
+
+
+def has_duplicates(s):
+    hist = histogram(s)
+    for val in hist.values():
+        if val > 1:
+            return True
+    return False
+
+
+def main():
+    for s in test_dups:
+        if has_duplicates(s):
+            print(s, "has duplicates")
+        else:
+            print(s, "has no duplicates")
+
+
+if __name__ == '__main__':
+    main()
---
+++ Part 2.py
@@ -0,0 +1,36 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+test_dups = ["zzz", "dog", "bookkeeper", "subdermatoglyphic", "subdermatoglyphics"]
+
+test_miss = ["zzz", "subdermatoglyphic", "the quick brown fox jumps over the lazy dog"]
+
+
+def histogram(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
+
+
+def missing_letters(s):
+    hist = histogram(s)
+    missing = ""
+    for letter in alphabet:
+        if letter not in hist:
+            missing += letter
+    if missing == "":
+        return s + " uses all the letters"
+    else:
+        return s + " is missing letters " + missing
+
+
+def main():
+    for s in test_miss:
+        print(missing_letters(s))
+
+
+if __name__ == '__main__':
+    main()
